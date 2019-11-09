1576601edede57ee72f74c1e8bb5e180637d16c9
charmas3r <evansmith0115@gmail.com>
2019-11-09 15:20:37 -0800

    initialiation & thread creation

--- main.cpp
+++ main.cpp
@@ -17,8 +17,61 @@ How To Run Program:
 #include <string>
 #include <cstring>
 #include <vector>
+#include <pthread.h>
+#include <semaphore.h>
+#include <algorithm>
 #include "buffer.h"
 using namespace std;
+
+
+pthread_mutex_t mutex;
+sem_t full;
+sem_t empty;
+
+
+void *producer(void *param) {
+
+//    buffer_item item;
+//
+//    while (true) {
+//        /* sleep for a random period of time */
+//
+////        sleep();
+//
+//        /* generate a random number */
+//
+//        item = rand();
+//
+//        if (buffer().insert_item(item)) {
+//            printf("report error condition");
+//        } else {
+//            printf("producer produced %d\n", item);
+//            break;
+//        }
+//
+//    }
+}
+
+void *consumer(void *param) {
+//
+//    buffer_item item;
+//
+//    while (true) {
+//
+//        /* sleep for a random period of time */
+//
+////        sleep(...);
+//
+//        if (buffer().remove_item(&item)) {
+//            printf("report error condition");
+//        } else {
+//            printf("consumer consumed %d\n",item);
+//            break; //delete
+//        }
+//    }
+
+}
+
 
 
 int main(int argc, char** argv) {
@@ -37,17 +90,52 @@ int main(int argc, char** argv) {
     if(argc <= 3 || argc >5)
     {
         cout << "three command-line arguments:\n";
-        cout << "- amount of time to run the program (in seconds, positive integer that is nonzero)\n";
+        cout << "- how long main thread should run before sleeping (in seconds, positive integer that is nonzero)\n";
         cout << "- number of producer threads to create (positive integer or zero)\n";
-        cout << "- number of consumer threads to create (positive integer or zero)\n";
+        cout << "- number of consumer threads to create (positive integer or zero)\n\n";
+        cout << "usage: ./assignment4 <time(s)> <#producer_threads> <#consumer_threads>\n";
     }
 
+    int main_thread_time;
+    int producer_thread_count;
+    int consumer_thread_count;
+
+    //comment out when done (testing purposes)
+    if(argc <= 1) {
+        cout << "\n\n";
+        cout << "Enter time: ";
+        cin >> main_thread_time;
+        cout << "Producer threads: ";
+        cin >> producer_thread_count;
+        cout << "Consumer  threads: ";
+        cin >> consumer_thread_count;
+    }
 
     /* 1. Get command line arguments argv[1],argv[2],argv[3] */
 
-    /* 2. Initialize buffer */
+    //uncomment later
+//    int main_thread_time = atoi(argv[1]);
+//    int producter_thread_count = atoi(argv[2]);
+//    int consumer_thread_count = atoi(argv[3]);
+
+    /* 2. Initialization */
+
+    pthread_t producer_threads[producer_thread_count];
+    pthread_t consumer_threads[consumer_thread_count];
+    sem_init(&empty, 0, BUFFER_SIZE);
 
     /* 3. Create producer thread(s) */
+
+    for (pthread_t producer_thread : producer_threads) {
+        pthread_create(&producer_thread, nullptr, producer, nullptr);
+        cout << "produced a producer thread" << endl;
+    }
+
+    for (pthread_t consumer_thread : consumer_threads) {
+        pthread_create(&consumer_thread, nullptr, consumer, nullptr);
+        cout << "produced a consumer thread" << endl;
+    }
+
 
     /* 4. Create consumer thread(s) */
 
@@ -59,43 +147,4 @@ int main(int argc, char** argv) {
 }
 
 
-void *producer(void *param) {
 
-    buffer_item item;
-
-    while (true) {
-        /* sleep for a random period of time */
-
-        sleep(...);
-
-        /* generate a random number */
-
-        item = rand();
-
-        if (buffer().insert_item(item)) {
-            printf("report error condition");
-        } else {
-            printf("producer produced %d\n", item);
-        }
-
-    }
-}
-
-void *consumer(void *param) {
-
-    buffer_item item;
-
-    while (true) {
-
-        /* sleep for a random period of time */
-
-        sleep(...);
-
-        if (buffer().remove_item(&item)) {
-            fprintf("report error condition");
-        } else {
-            printf("consumer consumed %d\n",item);
-        }
-    }
-
-}
